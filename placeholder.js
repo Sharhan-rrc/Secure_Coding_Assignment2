e01332b0fd0d0257b6cf174620d5f374e72db509
Sharhan Kaji <shabibbhaikaji@rrc.ca>
2026-02-09 11:17:15 -0600

    There was error for same function name

---
+++ placeholder.js
@@ -0,0 +1,8 @@
+// placeholder.js - simple JavaScript example
+
+function add(a, b) {
+  return a + b;
+}
+
+const result = add(2, 3);
+console.log(`Result is: ${result}`);
--- placeholder.ts
+++ placeholder.ts
@@ -6,3 +6,5 @@ function add(a: number, b: number): number {
 
 const result: number = add(2, 3);
 console.log(`Result is: ${result}`);
+
+export {};
